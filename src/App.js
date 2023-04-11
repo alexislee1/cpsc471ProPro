0f94ba754447fd4d450ff957edc67726bd065c1c
alexislee1 <47426034+alexislee1@users.noreply.github.com>
2023-04-10 20:20:58 -0600

    Where is this going to

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Axios from 'axios';
 
 function App() {
+
+  const[UserName, setUser] = useState("");
+  const[Password, setPassword] = useState("");
+
+  const submit = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      UserName: UserName, 
+      Password: Password,
+    }).then(() => {
+      alert('successful insert');
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <div className="intext0">
+          SIGN UP PAGE
+        </div>
+        <form>
+          <label htmlFor="uname">User Name:</label><br />
+          <input type="text" name="UserName" onChange={(e) => {
+            setUser(e.target.value);
+          }}/>
+          <br />
+          <br />
+          <label htmlFor="fname">Password:</label><br />
+          <input type="text" name="Password" onChange={(e) => {
+            setPassword(e.target.value);
+          }} />
+          <br />
+          <br />
+          <label htmlFor="fname">First name:</label><br />
+          <input type="text" id="fname" name="fname" />
+          <br />
+          <br />
+          <label htmlFor="lname">Last name:</label><br />
+          <input type="text" id="lname" name="lname" />
+          <br />
+          <br />
+          <label htmlFor="em">Email:</label><br />
+          <input type="text" id="em" name="pem" />
+        </form>
+        <br />
+        <button onClick = {submit}>Sign up</button>
+
+      </div>
+      
   );
 }
 
